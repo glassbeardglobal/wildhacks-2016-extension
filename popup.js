a0934291b82e5c8c50454d735a3637a969fbf897
Brian Luc <bluc96@gmail.com>
2016-11-19 13:08:32 -0600

    barebones popup

--- popup.js
+++ popup.js
@@ -1,9 +1,26 @@
 $(function() {
-    $("#BlackListButton").click(function() {
-        //add blacklist
+
+    $('#BlacklistButton').click(function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+            var activeTab = arrayOfTabs[0];
+            var url = new URL(activeTab.url);
+            var domain = url.hostname;
+
+            AddToBlacklist(domain)
+        })
     })
 
-    $('#ListButton').click(function() {
-        //see list
+    $('#Settings').click(function() {
+        //open settings tab
+        chrome.tabs.create({
+                url: chrome.extension.getURL('settings.html')
+            });
     })
 });
+
+function AddToBlacklist(domain) {
+    //TODO change URL
+    var url = "https://sjback.herokuapp.com/api/users/blacklist";
+    var data = {userid: 'temp', site: domain};
+    $.post(url, data);
+}
