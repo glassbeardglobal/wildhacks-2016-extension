bec14e6f617f816ec0c83dd65cd6cb519ead51a0
Brian Luc <bluc96@gmail.com>
2016-11-19 13:26:43 -0600

    barebones backgroundjs

--- background.js
+++ background.js
@@ -1,27 +1,17 @@
-//TODO
-/*
-Browser Action: add website to list of banned websites
-EventPage: check that new loaded website isn't in list
-if it is in list, programatically inject html/css
-*/
-
 $(function() {
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         if (changeInfo.url != undefined) {
-            var url = new URL(changeInfo.url);
+            //grab domain
+            var url = new URL(changeInfo.url)
             var domain = url.hostname;
-
-            //send domain to server
+            //send domain
             sendDomain(domain);
         }
-    }
-})
-
+    });
+});
 
 function sendDomain(domain) {
-    //TODO replace url with server url
-    var url = "temp";
-    $.post(url, domain);
-
-    //TODO handle cases where this doesn't reach server
+    var url = 'https://sjback.herokuapp.com/api/users/addpage';
+    var data = {userid: "temp", site: domain};
+    $.post(url, data)
 }
